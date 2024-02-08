f1953dbfc7ea49bf658ec50fe70a03b957918367
Cian Thornhill <cianthornhill95@gmail.com>
2024-02-08 10:28:27 +0000

    resolved merge conflicts

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,50 +1,3 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 3dde6300a91ac3a4e144dc7e322947131caa6492
-'use strict';
-
-// Get all cards
-var cards = document.querySelectorAll('.pokemon-type-card');
-
-// Function to handle mouseenter event
-function handleMouseEnter(event) {
-    if (window.innerWidth >= 768) {
-        var descriptions = this.querySelector('.descriptions');
-        descriptions.classList.add('show-descriptions');
-        event.target.setPointerCapture(event.pointerId);
-    }
-}
-
-// Function to handle mouseleave event
-function handleMouseLeave(event) {
-    if (window.innerWidth >= 768) {
-        var descriptions = this.querySelector('.descriptions');
-        descriptions.classList.remove('show-descriptions');
-        event.target.releasePointerCapture(event.pointerId);
-    }
-}
-
-// Add event listeners to each card
-cards.forEach(function(card) {
-    // Add event listener for mouseenter (hover) on larger screens
-    card.addEventListener('mouseenter', handleMouseEnter);
-
-    // Add event listener for mouseleave (hover out) on larger screens
-    card.addEventListener('mouseleave', handleMouseLeave);
-
-    // Add event listener for tap on mobile devices
-    card.addEventListener('click', function() {
-        if (window.innerWidth < 768) {
-            var descriptions = this.querySelector('.descriptions');
-            descriptions.classList.toggle('show-descriptions');
-        }
-    });
-<<<<<<< HEAD
-});
-=======
-});
-=======
 
 /*Functions to handle sidebar functionality */
 
@@ -59,8 +12,5 @@ function hideSidebar(){
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'none';
 }
->>>>>>> 820c58935ffe1a4b368d2a7aa9ee0112df9164b9
->>>>>>> 3dde6300a91ac3a4e144dc7e322947131caa6492
-
 
 /*-----------------------------------------*/
